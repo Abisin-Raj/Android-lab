1e403cb0ecc451decd31e4165cd80200b7aa9e88
Abisin-Raj <abisinr201@gmail.com>
2026-02-22 23:35:16 +0530

    Add MainActivity.java for program12

---
+++ program12/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.program12;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	private static int imgloaded = 1;
+	String imgPath;
+	ImageView imgView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+	public void loadImagefromGallery(View view)	{
+		Intent galleryIntent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		startActivityForResult(galleryIntent, imgloaded);
+	}
+	
+	@Override
+	protected void onActivityResult (int requestCode,int resultCode,Intent data){
+		super.onActivityResult(requestCode, resultCode, data);
+		try{
+			if(requestCode == imgloaded && resultCode == RESULT_OK && null != data)	{
+				Uri selectedImage = data.getData();
+				String[] filePathColumn = {
+						MediaStore.Images.Media.DATA
+				};
+				Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+				cursor.moveToFirst();
+				
+				int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+				imgPath = cursor.getString(columnIndex);
+				cursor.close();
+				imgView.setImageBitmap(BitmapFactory.decodeFile(imgPath));
+			}
+			else{
+				Toast.makeText(this, "No images selected", Toast.LENGTH_LONG).show();
+			}
+		}
+		catch(Exception e){
+			Toast.makeText(this, "something went wrong", Toast.LENGTH_LONG).show();
+		}
+	}
+
+	
+}
